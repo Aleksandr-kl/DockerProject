166a369bcfada3ee6040bb8932655342afb88a2e
Aleksandr-kl <kalenskuy@gmail.com>
2023-08-30 22:45:02 +0300

    added main

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,13 +1,22 @@
+
 function Footer() {
-    return <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <p className="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
-        <ul className="nav col-md-4 justify-content-end">
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
-        </ul>
-    </footer>
+    return (
+        <div className="page-container">
+            <div className="content">
+                {/* Ваш контент сторінки */}
+            </div>
+            <footer className="footer-container d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <p className="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
+                <ul className="nav col-md-4 justify-content-end">
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary footer-link">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary footer-link">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary footer-link">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary footer-link">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary footer-link">About</a></li>
+                </ul>
+            </footer>
+        </div>
+    );
 }
+
 export default Footer;
--- frontend/src/components/Page/Page.js
+++ frontend/src/components/Page/Page.js
@@ -1,11 +1,16 @@
 import './Page.css';
 import Header from "../Header";
 import Footer from "../Footer";
+import Main from "../Main";
+
+
 function Page(props) {
     return <div>
         <Header/>
+        <Main/>
         <Footer/>
     </div>
 
 }
+
 export default Page;
