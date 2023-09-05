9f6e841e20acf53ffd7ce49eaaf05dd7dd0932d6
Aleksandr-kl <kalenskuy@gmail.com>
2023-09-05 20:00:17 +0300

    Update Contact, App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import {Link,Route,Routes} from "react-router-dom";
+import {Link, Route, Routes, useParams} from "react-router-dom";
 import Home from "./components/Home";
 import Contacts from "./components/Contacts";
 import About from "./components/About";
 
 function App() {
+
   return (
     <div className="App">
-
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/contacts/:number" element={<Contacts/>}/>
         <Route path="*" element={<h3>404</h3>}/>
       </Routes>
     </div>
--- frontend/src/components/Contacts.js
+++ frontend/src/components/Contacts.js
@@ -1,4 +1,8 @@
+import {useParams} from "react-router-dom";
+
 function Contacts() {
+    const params=useParams();
+    const number=params.number;
     return(
         <div>
             <h1>Contacts Page</h1>
